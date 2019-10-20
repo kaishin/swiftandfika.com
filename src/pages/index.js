bc906a510f5660af37b8b816d5d21d8ab2e05048
Reda Lemeden <git@redalemeden.com>
2019-10-20 23:05:18 +0200

    Add mailing list form

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,21 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <ThreeDeeView />
+    <form
+      action="https://buttondown.email/api/emails/embed-subscribe/swiftandfika"
+      method="post"
+      target="popupwindow"
+      onsubmit="window.open('https://buttondown.email/swiftandfika', 'popupwindow')"
+      class="embeddable-buttondown-form"
+    >
+      <label for="bd-email">Enter your email</label>
+      <input type="email" name="email" id="bd-email" />
+      <input type="hidden" value="1" name="embed" />
+      <input type="submit" value="Subscribe" />
+      <p>
+        <a href="https://buttondown.email">Powered by Buttondown.</a>
+      </p>
+    </form>
   </Layout>
 );
 
