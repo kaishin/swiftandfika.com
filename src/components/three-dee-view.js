401080494c0b94ec11e294bbf8397173eefcb4dd
Reda Lemeden <git@redalemeden.com>
2020-01-03 01:35:11 +0100

    Add orbital controls

--- src/components/three-dee-view.js
+++ src/components/three-dee-view.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 import GLTFLoader from 'three-gltf-loader';
 
+var OrbitControls = require('three-orbit-controls')(THREE);
+
 export default class ThreeDeeView extends Component {
   constructor(props) {
     super(props);
@@ -12,21 +14,23 @@ export default class ThreeDeeView extends Component {
     this.setupCamera();
     this.setupRenderer();
     this.setupScene();
-    this.setupLights();
+    this.setupControls();
     this.animate();
   };
 
   setupCamera = () => {
-    let { innerWidth: width, innerHeight: height } = window;
-    this.camera = new THREE.PerspectiveCamera(105, width / height, 0.1, 1000);
-    this.camera.position.z = 5;
+    this.camera = new THREE.PerspectiveCamera(80, 1 / 0.6, 0.2, 800);
+    this.camera.position.z = 3.5;
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
   };
 
   setupRenderer = () => {
     let { innerWidth: width, innerHeight: height } = window;
+    let availableWidth = Math.min(800, width);
+    let customHeight = availableWidth * 0.6;
 
     this.renderer = new THREE.WebGLRenderer({
-      alpha: false,
+      // alpha: true,
       antialias: false,
       canvas: document.querySelector('#gl-view'),
       clearAlpha: 0.25,
@@ -34,24 +38,17 @@ export default class ThreeDeeView extends Component {
       failIfMajorPerformanceCaveat: true,
     });
 
-    this.renderer.setClearColor(0xebebeb);
     this.renderer.setPixelRatio(Math.min(1.25, window.devicePixelRatio));
-    this.renderer.setSize(width, height);
+    this.renderer.setSize(availableWidth, customHeight);
   };
 
   setupScene = () => {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x039b4f });
-    this.cube = new THREE.Mesh(geometry, material);
-
-    this.scene.background = new THREE.Color(0x9a3e00);
-
     this.loader = new GLTFLoader();
 
     this.loader.load(
       'coffee/scene.gltf',
       (gltf) => {
-        const material = new THREE.MeshBasicMaterial({ color: 0xaa88ff, wireframe: true });
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
         gltf.scene.traverse(function(child) {
           if (child.isMesh) {
             child.material = material;
@@ -66,25 +63,22 @@ export default class ThreeDeeView extends Component {
         console.error(error);
       },
     );
-
-    // this.scene.add(this.cube);
   };
 
-  setupLights = () => {
-    let aLight = new THREE.AmbientLight(0xebebeb);
-    let dLight = new THREE.DirectionalLight(0x6695f7, 1, 100);
-    dLight.position.set(0, 0, 100);
-
-    this.scene.add(aLight);
-    this.scene.add(dLight);
+  setupControls = () => {
+    this.controls = new OrbitControls(this.camera);
+    this.controls.enableDamping = true;
+    this.controls.enablePan = false;
+    this.controls.enableZoom = false;
+    this.controls.minPolarAngle = Math.PI / 2.5;
+    this.controls.maxPolarAngle = Math.PI / 2.5;
   };
 
   animate = () => {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.controls.update();
 
     if (this.cup) {
-      this.cup.rotation.x = 1;
+      this.cup.rotation.x = 0.2;
       this.cup.rotation.y += 0.005;
     }
 
@@ -96,3 +90,9 @@ export default class ThreeDeeView extends Component {
     return <canvas id="gl-view" className="3d-shape" />;
   }
 }
+
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize( window.innerWidth, window.innerHeight );
+// }
