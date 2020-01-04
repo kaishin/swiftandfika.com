8e231cbf27ed632380affa7bb0affcd3f3214231
Reda Lemeden <git@redalemeden.com>
2020-01-04 13:43:10 +0100

    Leave resizing to CSS

--- src/components/three-dee-view.js
+++ src/components/three-dee-view.js
@@ -7,9 +7,9 @@ var OrbitControls = require('three-orbit-controls')(THREE);
 export default class ThreeDeeView extends Component {
   constructor(props) {
     super(props);
-
     this.scene = new THREE.Scene();
   }
+
   componentDidMount = () => {
     this.setupCamera();
     this.setupRenderer();
@@ -19,16 +19,16 @@ export default class ThreeDeeView extends Component {
   };
 
   setupCamera = () => {
-    this.camera = new THREE.PerspectiveCamera(80, 1 / 0.6, 0.2, 800);
+    let fov = 75;
+    let aspect = 2;
+    let near = 0.2;
+    let far = 1000;
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     this.camera.position.z = 3.5;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
   };
 
   setupRenderer = () => {
-    let { innerWidth: width, innerHeight: height } = window;
-    let availableWidth = Math.min(800, width);
-    let customHeight = availableWidth * 0.6;
-
     this.renderer = new THREE.WebGLRenderer({
       // alpha: true,
       antialias: false,
@@ -37,9 +37,6 @@ export default class ThreeDeeView extends Component {
       preserveDrawingBuffer: false,
       failIfMajorPerformanceCaveat: true,
     });
-
-    this.renderer.setPixelRatio(Math.min(1.25, window.devicePixelRatio));
-    this.renderer.setSize(width, height);
   };
 
   setupScene = () => {
@@ -74,12 +71,32 @@ export default class ThreeDeeView extends Component {
     this.controls.maxPolarAngle = Math.PI / 2.5;
   };
 
+  resizeRendererToDisplaySize = () => {
+    const canvas = this.renderer.domElement;
+    const pixelRatio = window.devicePixelRatio
+    const width = canvas.clientWidth * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const isResizeNeeded = canvas.width !== width || canvas.height !== height;
+
+    if (isResizeNeeded) {
+      this.renderer.setSize(width, height, false);
+    }
+
+    return isResizeNeeded;
+  };
+
   animate = () => {
     this.controls.update();
 
     if (this.cup) {
       this.cup.rotation.x = 0.2;
       this.cup.rotation.y += 0.005;
+    }
+
+    if (this.resizeRendererToDisplaySize()) {
+      const canvas = this.renderer.domElement;
+      this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      this.camera.updateProjectionMatrix();
     }
 
     requestAnimationFrame(this.animate);
