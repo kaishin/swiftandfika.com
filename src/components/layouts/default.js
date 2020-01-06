e026d4c788ba189bc6cd98aab55a4eb4a41059e6
Reda Lemeden <git@redalemeden.com>
2020-01-06 23:59:39 +0100

    Add CSS

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -2,7 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
-import "./layout.css"
+import "../../css/normalize.css"
+import "../../css/styles.css"
 
 const Layout = ({ children }) => {
   // const data = useStaticQuery(graphql`
