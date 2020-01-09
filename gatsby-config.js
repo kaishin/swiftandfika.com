6bb877f9a384cb6d0a3774d00b02005ee1202ec7
Reda Lemeden <git@redalemeden.com>
2020-01-09 11:32:19 +0100

    Remove unused config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'images',
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
   ],
